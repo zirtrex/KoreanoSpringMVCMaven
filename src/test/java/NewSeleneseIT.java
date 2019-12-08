5edbc9904641d79940f4106c38f18f36db80d875
Rafael Contreras <zirtrex@live.com>
2019-12-08 07:20:31 -0500

    Editando NewSeleneseIT

--- src/test/java/NewSeleneseIT.java
+++ src/test/java/NewSeleneseIT.java
@@ -83,7 +83,7 @@ public class NewSeleneseIT {
     @Test
     public void testEditarEquipo() {
 
-        driver.get("http://localhost:8080/KoreanoSpringMVCMaven/editar-equipo.htm?codEquipo=1");
+        driver.get("http://localhost:8282/KoreanoSpringMVCMaven/editar-equipo.htm?codEquipo=1");
         
         WebDriverWait wait = new WebDriverWait(driver, 10);        
         
